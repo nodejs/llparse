4f5e93c80d67f2a8c9be240a9cb4150bdd30bdd5
Fedor Indutny <fedor@indutny.com>
2018-02-11 19:04:18 -0500

    save

--- test/fixtures/example.js
+++ test/fixtures/example.js
@@ -29,6 +29,7 @@ const init = () => {
   switch (_) {
     default:
       rerun(start_req_or_res);
+      break;
   }
 };
 
@@ -36,21 +37,25 @@ const start_req_or_res = () => {
   switch (_) {
     case [ 0x0a, 0x0d ]:
       skip();
+      break;
 
     '@notify-on-start(on_message_begin)';
     case METHODS:
       state.type = HTTP_REQUEST;
       state.method = match();
       next(request_after_method);
+      break;
 
     '@notify-on-start(on_message_begin)';
     case 'HTTP':
       state.type = HTTP_RESPONSE;
       next(response_slash);
+      break;
 
     '@unlikely';
     default:
       error(INVALID_METHOD, 'Unknown method');
+      break;
   }
 };
 
@@ -58,14 +63,17 @@ const request_after_method = () => {
   switch (_) {
     case ' ':
       skip();
+      break;
 
     '@unlikely';
     case 0x0:
       error(INVALID_METHOD, '`\0` after method');
+      break;
 
     default:
       settings.on_url.start();
       rerun(url);
+      break;
   }
 };
 
@@ -74,17 +82,20 @@ const url = () => {
     case ' ':
       next(req_http_start);
       settings.on_url.end();
+      break;
 
     '@unlikely';
     case [ '\r', '\n' ]:
       error(INVALID_URL_CHARACTER,
             'URL can\'t have newline chars in it');
+      break;
 
     '@unlikely';
     '@mode(strict)';
     case [ '\t', '\f' ]:
       error(INVALID_URL_CHARACTER,
             'URL can\'t have "\\t" or "\\f" chars in it');
+      break;
   }
 };
 
