6b950221a055c49c9fe3d623cf06bef8e886dc94
Fedor Indutny <fedor@indutny.com>
2018-02-20 23:17:38 -0500

    span: save progress

--- test/span-test.js
+++ test/span-test.js
@@ -15,14 +15,23 @@ describe('LLParse/span', () => {
 
   it('should invoke span callback', (callback) => {
     const start = p.node('start');
-    const body = p.node('body');
-    const span = p.span('on_data');
+    const dot = p.node('dot');
+    const dash = p.node('dash');
+    const span = {
+      dot: p.span('on_dot'),
+      dash: p.span('on_dash')
+    };
 
-    start.otherwise(span.start().otherwise(body));
+    start.otherwise(span.dot.start(dot));
 
-    body
-      .match([ '0', '1', '2' ], body)
-      .otherwise(span.end().otherwise(start));
+    dot
+      .match('.', dot)
+      .match('-', span.dash.start(dash))
+      .skipTo(span.dot.end(start));
+
+    dash
+      .match('-', dash)
+      .otherwise(span.dash.end(dot));
 
     const binary = fixtures.build('span', p.build(start));
 
@@ -36,5 +45,14 @@ describe('LLParse/span', () => {
     start.otherwise(span.start().otherwise(start));
 
     assert.throws(() => p.build(start), /loop/);
+  });
+
+  it('should throw on unmatched ends', () => {
+    const start = p.node('start');
+    const span = p.span('on_data');
+
+    start.otherwise(span.end().otherwise(start));
+
+    assert.throws(() => p.build(start), /unmatched/i);
   });
 });
