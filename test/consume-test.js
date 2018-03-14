6b273c80ae4407e45b3791fe18ed3c8d8d250619
Fedor Indutny <fedor@indutny.com>
2018-03-13 20:58:39 -0400

    consume: fix for i64 or larger field types

--- test/consume-test.js
+++ test/consume-test.js
@@ -15,8 +15,29 @@ describe('LLParse/consume', function() {
     p = llparse.create('llparse');
   });
 
-  it('should consume bytes', (callback) => {
+  it('should consume bytes with i8 field', (callback) => {
     p.property('i8', 'to_consume');
+
+    const start = p.node('start');
+    const consume = p.consume('to_consume');
+
+    start.select({
+      '0': 0, '1': 1, '2': 2, '3': 3, '4': 4
+    }, p.invoke(p.code.store('to_consume'), consume));
+
+    start
+      .otherwise(p.error(1, 'unexpected'));
+
+    consume
+      .otherwise(printOff(p, start));
+
+    const binary = fixtures.build(p, start, 'consume');
+
+    binary('3aaa2bb1a01b', 'off=4\noff=7\noff=9\noff=10\noff=12\n', callback);
+  });
+
+  it('should consume bytes with i64 field', (callback) => {
+    p.property('i64', 'to_consume');
 
     const start = p.node('start');
     const consume = p.consume('to_consume');
