d0425f8b525139a86b9ba8ac6c75f4e6ea78331c
Fedor Indutny <fedor@indutny.com>
2018-03-17 19:40:27 -0400

    span-end: implementation

--- src/node/span-end.ts
+++ src/node/span-end.ts
@@ -1,5 +1,8 @@
+import * as assert from 'assert';
+
 import { Span as SpanCallback } from '../code';
-import { IRBasicBlock } from '../compilation';
+import { IRBasicBlock, IRValue } from '../compilation';
+import { GEP_OFF } from '../constants';
 import { Span } from '../span';
 import { IUniqueName } from '../utils';
 import { INodePosition, Node } from './base';
@@ -11,7 +14,54 @@ export class SpanEnd extends Node {
   }
 
   protected doBuild(bb: IRBasicBlock, pos: INodePosition): void {
-    // TODO(indutny): implement me
-    this.pause(bb);
+    const ctx = this.compilation;
+
+    // Load
+    const startPtr = ctx.spanPosField(bb, this.span.index);
+    const start = bb.load(startPtr);
+
+    // ...and reset
+    bb.store(start.ty.toPointer().val(null), startPtr);
+
+    const call = bb.call(this.callback.build(ctx), [
+      ctx.stateArg(bb),
+      start,
+      pos.current,
+    ]);
+
+    // Check return value
+    const errorCmp = bb.icmp('eq', call, call.ty.toInt().val(0));
+
+    const { onTrue: noError, onFalse: error } = ctx.branch(bb, errorCmp, {
+      onFalse: 'unlikely',
+      onTrue: 'likely',
+    });
+    noError.name = 'no_error';
+    error.name = 'error';
+
+    // Handle error
+    this.buildError(error, pos, call);
+  }
+
+  private buildError(bb: IRBasicBlock, pos: INodePosition, code: IRValue): void {
+    const ctx = this.compilation;
+
+    const reason = ctx.cstring('Span callback error');
+    const cast = bb.getelementptr(reason, GEP_OFF.val(0), GEP_OFF.val(0),
+      true);
+
+    const errorField = ctx.errorField(bb);
+    bb.store(ctx.truncate(bb, code, errorField.ty), errorField);
+    bb.store(cast, ctx.reasonField(bb));
+
+    const otherwise = this.otherwise!;
+    bb.store(otherwise.noAdvance ? pos.current : pos.next,
+      ctx.errorPosField(bb));
+
+    const resumptionTarget = otherwise.node.build(ctx);
+    bb.store(resumptionTarget, ctx.currentField(bb));
+    ctx.addResumptionTarget(resumptionTarget);
+
+    bb.ret(bb.parent.ty.toSignature().returnType.toPointer().val(null));
   }
 }
