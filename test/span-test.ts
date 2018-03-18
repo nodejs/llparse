4746adf3cb42e24a116f4de25d84bf41bc889693
Fedor Indutny <fedor@indutny.com>
2018-03-18 17:39:09 -0400

    test/span: port rest

--- test/span-test.ts
+++ test/span-test.ts
@@ -1,8 +1,10 @@
+import * as assert from 'assert';
+
 import { LLParse } from '../src/api';
 
 import { build, printMatch, printOff } from './fixtures';
 
-describe('llparse/Compiler', () => {
+describe('llparse/spans', () => {
   let p: LLParse;
 
   beforeEach(() => {
@@ -42,5 +44,57 @@ describe('llparse/Compiler', () => {
       'off=2 len=2 span[dash]="--"\n' +
       'off=6 len=2 span[underscore]="__"\n' +
       'off=0 len=10 span[dot]="..--..__.."\n');
+  });
+
+  it('should throw on loops', () => {
+    const start = p.node('start');
+    const span = p.span(p.code.span('llparse__on_data'));
+
+    start.otherwise(span.start().skipTo(start));
+
+    assert.throws(() => p.build(start), /loop.*on_data/);
+  });
+
+  it('should throw on unmatched ends', () => {
+    const start = p.node('start');
+    const span = p.span(p.code.span('llparse__on_data'));
+
+    start.otherwise(span.end().skipTo(start));
+
+    assert.throws(() => p.build(start), /unmatched.*on_data/i);
+  });
+
+  it('should propagate through the Invoke map', () => {
+    const start = p.node('start');
+    const span = p.span(p.code.span('llparse__on_data'));
+
+    p.property('i8', 'custom');
+
+    start.otherwise(p.invoke(p.code.load('custom'), {
+      0: span.end().skipTo(start),
+    }, span.end().skipTo(start)));
+
+    assert.doesNotThrow(() => p.build(span.start(start)));
+  });
+
+  it('should return error', async () => {
+    const start = p.node('start');
+    const dot = p.node('dot');
+
+    const span = {
+      pleaseFail: p.span(p.code.span('llparse__please_fail')),
+    };
+
+    start.otherwise(span.pleaseFail.start(dot));
+
+    dot
+      .match('.', dot)
+      .skipTo(span.pleaseFail.end(start));
+
+    const binary = build(p, start, 'span-error');
+
+    await binary.check(
+      '....a',
+      /off=\d+ error code=1 reason="Span callback error"\n/);
   });
 });
