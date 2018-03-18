b03e1af33a8b073030d42211d5d6891f0e573941
Fedor Indutny <fedor@indutny.com>
2018-03-18 18:25:53 -0400

    test: port transform test

---
+++ test/transform-test.ts
@@ -0,0 +1,26 @@
+import * as assert from 'assert';
+
+import { LLParse } from '../src/api';
+
+import { build, printMatch, printOff } from './fixtures';
+
+describe('llparse/transform', () => {
+  let p: LLParse;
+
+  beforeEach(() => {
+    p = new LLParse();
+  });
+
+  it('should apply transformation before the match', async () => {
+    const start = p.node('start');
+
+    start
+      .transform(p.transform.toLowerUnsafe())
+      .match('connect', printOff(p, start))
+      .match('close', printOff(p, start))
+      .otherwise(p.error(1, 'error'));
+
+    const binary = build(p, start, 'transform-lower');
+    await binary.check('connectCLOSEcOnNeCt', 'off=7\noff=12\noff=19\n');
+  });
+});
