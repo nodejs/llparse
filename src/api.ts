2568627f8ede33203d1fb4b03b887e99bf6e8742
Fedor Indutny <fedor@indutny.com>
2018-03-18 22:27:56 -0400

    api: moar exported classes

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,8 @@
-import { Builder, node } from 'llparse-builder';
+import { Builder, node, Span } from 'llparse-builder';
 
 import { Compiler, ICompilerOptions, ICompilerResult } from './compiler';
 
-export { node };
+export { node, Span };
 
 export class LLParse extends Builder {
   constructor(private readonly prefix: string = 'llparse') {
