99f60d733caf19827f0ab6b00d6b00018e05b353
Fedor Indutny <fedor@indutny.com>
2018-08-08 18:15:33 -0400

    c: implement `consume`

--- src/implementation/c/node/consume.ts
+++ src/implementation/c/node/consume.ts
@@ -5,12 +5,25 @@ import { Node } from './base';
 
 export class Consume extends Node<frontend.node.Consume> {
   public doBuild(out: string[]): void {
-    const otherwise = this.ref.otherwise!;
+    const ctx = this.compilation;
 
-    if (!otherwise.noAdvance) {
-      this.prologue(out);
-    }
+    const index = ctx.indexField();
 
-    this.tailTo(out, otherwise);
+    out.push('size_t avail;');
+    out.push('size_t need;');
+    out.push('');
+    out.push(`avail = ${ctx.endPosArg()} - ${ctx.posArg()};`);
+    out.push(`need = ${index};`);
+    out.push('if (avail >= need) {');
+    out.push(`  p += need;`);
+    out.push(`  ${index} = 0;`);
+    const tmp: string[] = [];
+    this.tailTo(tmp, this.ref.otherwise!);
+    ctx.indent(out, tmp, '  ');
+    out.push('}');
+    out.push('');
+
+    out.push(`${index} -= avail;`);
+    this.pause(out);
   }
 }
