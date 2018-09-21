4a14f23c2dd0366329a3441dfa31f798ca079922
Fedor Indutny <fedor@indutny.com>
2018-09-21 11:53:46 +0400

    c: implement span nodes

--- src/implementation/c/node/span-end.ts
+++ src/implementation/c/node/span-end.ts
@@ -1,16 +1,59 @@
 import * as frontend from 'llparse-frontend';
 
 import { Compilation } from '../compilation';
+import { STATE_ERROR } from '../constants';
 import { Node } from './base';
 
 export class SpanEnd extends Node<frontend.node.SpanEnd> {
   public doBuild(out: string[]): void {
-    const otherwise = this.ref.otherwise!;
+    out.push('const unsigned char* start;');
+    out.push('int err;');
+    out.push('');
 
+    this.prologue(out);
+
+    const ctx = this.compilation;
+    const field = this.ref.field;
+    const posField = ctx.spanPosField(field.index);
+
+    // Load start position
+    out.push(`start = ${posField};`);
+
+    // ...and reset
+    out.push(`${posField} = NULL;`);
+
+    // Invoke callback
+    const callback = ctx.buildCode(ctx.unwrapCode(this.ref.callback));
+    out.push(`err = ${callback}(${ctx.stateArg()}, start, ${ctx.posArg()});`);
+
+    out.push('if (err != 0) {');
+    const tmp: string[] = [];
+    this.buildError(tmp, 'err');
+    ctx.indent(out, tmp, '  ');
+    out.push('}');
+
+    const otherwise = this.ref.otherwise!;
+    this.tailTo(out, otherwise);
+  }
+
+  private buildError(out: string[], code: string) {
+    const ctx = this.compilation;
+
+    out.push(`${ctx.errorField()} = ${code};`);
+    out.push(`${ctx.reasonField()} = "Span callback error";`);
+
+    const otherwise = this.ref.otherwise!;
+    let resumePos = ctx.posArg();
     if (!otherwise.noAdvance) {
-      this.prologue(out);
+      resumePos = `(${resumePos} + 1)`;
     }
 
-    this.tailTo(out, otherwise);
+    out.push(`${ctx.errorPosField()} = ${resumePos};`);
+
+    const resumptionTarget = ctx.unwrapNode(otherwise.node).build(ctx);
+    out.push(`${ctx.currentField()} = ` +
+        `(void*) (intptr_t) ${resumptionTarget};`);
+
+    out.push(`return ${STATE_ERROR};`);
   }
 }
--- src/implementation/c/node/span-start.ts
+++ src/implementation/c/node/span-start.ts
@@ -5,12 +5,21 @@ import { Node } from './base';
 
 export class SpanStart extends Node<frontend.node.SpanStart> {
   public doBuild(out: string[]): void {
-    const otherwise = this.ref.otherwise!;
+    this.prologue(out);
 
-    if (!otherwise.noAdvance) {
-      this.prologue(out);
+    const ctx = this.compilation;
+    const field = this.ref.field;
+
+    const posField = ctx.spanPosField(field.index);
+    out.push(`${posField} = ${ctx.posArg()};`);
+
+    if (field.callbacks.length > 1) {
+      const cbField = ctx.spanCbField(field.index);
+      const callback = ctx.unwrapCode(this.ref.callback);
+      out.push(`${cbField} = ${ctx.buildCode(callback)}`);
     }
 
+    const otherwise = this.ref.otherwise!;
     this.tailTo(out, otherwise);
   }
 }
