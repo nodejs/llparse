8a9c9d69801b384a8c52e5914f97f5e39e767398
Fedor Indutny <fedor@indutny.com>
2019-02-01 09:22:30 -0500

    js: better use of bigint for consume

--- src/implementation/js/node/consume.ts
+++ src/implementation/js/node/consume.ts
@@ -8,12 +8,16 @@ export class Consume extends Node<frontend.node.Consume> {
     const ctx = this.compilation;
 
     const index = ctx.stateField(this.ref.field);
+    const ty = ctx.getFieldType(this.ref.field);
 
-    out.push(`const avail = ${ctx.bufArg()}.length - ${ctx.offArg()};`);
+    out.push(`let avail = ${ctx.bufArg()}.length - ${ctx.offArg()};`);
     out.push(`const need = ${index};`);
+    if (ty === 'i64') {
+      out.push('avail = BigInt(avail);');
+    }
     out.push('if (avail >= need) {');
-    out.push(`  off += need;`);
-    out.push(`  ${index} = 0;`);
+    out.push(`  off += ${ ty === 'i64' ? 'Number(need)' : 'need' };`);
+    out.push(`  ${index} = ${ ty === 'i64' ? '0n' : '' };`);
 
     const tmp: string[] = [];
     this.tailTo(tmp, this.ref.otherwise!);
