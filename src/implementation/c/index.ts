fb0edf0da014961e51b0e9fb5de4c2f6afab9608
Fedor Indutny <fedor@indutny.com>
2019-07-02 15:02:42 -0700

    c: vectorize table lookups
    Use SSE4.2 vector instructions to speed up matching. When node has a lot
    characters to match and loops to itself, the table lookups could be
    replaced with a call to `_mm_cmpestri`, skipping all matching characters
    in the input stream.

--- src/implementation/c/index.ts
+++ src/implementation/c/index.ts
@@ -36,6 +36,24 @@ export class CCompiler {
     out.push('#include <stdint.h>');
     out.push('#include <string.h>');
     out.push('');
+
+    // NOTE: Inspired by https://github.com/h2o/picohttpparser
+    out.push('#ifdef __SSE4_2__');
+    out.push(' #ifdef _MSC_VER');
+    out.push('  #include <nmmintrin.h>');
+    out.push(' #else  /* !_MSC_VER */');
+    out.push('  #include <x86intrin.h>');
+    out.push(' #endif  /* _MSC_VER */');
+    out.push('#endif  /* __SSE4_2__ */');
+    out.push('');
+
+    out.push('#ifdef _MSC_VER');
+    out.push(' #define ALIGN(n) _declspec(align(n))');
+    out.push('#else  /* !_MSC_VER */');
+    out.push(' #define ALIGN(n) __attribute__((aligned(n)))');
+    out.push('#endif  /* _MSC_VER */');
+
+    out.push('');
     out.push(`#include "${this.options.header || info.prefix}.h"`);
     out.push(``);
     out.push(`typedef int (*${info.prefix}__span_cb)(`);
